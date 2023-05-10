687847b453e09062a4d91956c1243f9cd531f385
Louw-C <74646083+Louw-C@users.noreply.github.com>
2023-05-11 08:41:00 +0900

    Tidyverse code

--- Code/Tidyverse.R
+++ Code/Tidyverse.R
@@ -2,3 +2,19 @@
 
 install.packages("tidyverse")
 library(tidyverse)
+
+#Some helpful bits to check tidyverse
+tidyverse_conflicts()#Conflicts between tidyverse and other packages
+tidyverse_deps()#List all tidyverse dependencies
+tidyverse_packages()#List all tidyverse packages
+tidyverse_update()#Update tidyverse packages
+
+# %>% = and then
+#shortcut ctrl+Shift+m
+#data frame 'and then' aka %>% and then your command
+
+
+#filter() allows you to select a subset of rows in a data frame. 
+
+#arrange() sorts the observations in a dataset in ascending or descending order
+#based on one of its variables.
